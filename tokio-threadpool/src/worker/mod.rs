tureus/tokio

commit 990186ec9d396a3cd2111c934d4dfc81d2aa866f

Optimize spinning in Worker::run (#470)

tokio-threadpool/src/worker/mod.rs
@@ -25,6 +25,7 @@ use std::marker::PhantomData;
 use std::rc::Rc;
 use std::sync::atomic::Ordering::{AcqRel, Acquire};
 use std::sync::Arc;
+use std::thread;
 use std::time::{Duration, Instant};
 
 /// Thread worker
@@ -216,6 +217,7 @@ impl Worker {
     ///
     /// This function blocks until the worker is shutting down.
     pub fn run(&self) {
+        const MAX_SPINS: usize = 60;
         const LIGHT_SLEEP_INTERVAL: usize = 32;
 
         // Get the notifier.
@@ -254,19 +256,24 @@ impl Worker {
             }
 
             if !consistent {
+                thread::yield_now();
                 spin_cnt = 0;
                 continue;
             }
 
-            // Starting to get sleeeeepy
-            if spin_cnt < 61 {
-                spin_cnt += 1;
-            } else {
-                tick = 0;
+            spin_cnt += 1;
 
-                if !self.sleep() {
-                    return;
-                }
+            if spin_cnt < MAX_SPINS {
+                thread::yield_now();
+                continue;
+            }
+
+            tick = 0;
+            spin_cnt = 0;
+
+            // Starting to get sleeeeepy
+            if !self.sleep() {
+                return;
             }
 
             // If there still isn't any work to do, shutdown the worker?

commit a39e8ce0eb02d23e03081e51a142aea98fa1dc89

threadpool: Remove yield when spinning workers to fix lost wakeups
## Motivation

In versions of `tokio-threadpool` > 0.1.4, an issue exists where the
threadpool will eventually fail to wake any worker threads, resulting in
a deadlock (issue #525). I used `git bisect` with the reproduction for
this issue I wrote (https://github.com/hawkw/tokio-deadlock-repro), and
determined that the issue was introduced in
990186ec9d396a3cd2111c934d4dfc81d2aa866f. Some manual testing indicated
that the addition of calls to `std::thread::yield_now` in `Worker::run`
were linked with the lost wakeups, and that disabling them seemed to
resolve the issue.

## Solution

I've removed the call to `thread::yield_now` in `Worker::run`.

Initially, I found that not yielding resulted in significantly worse
performance on the `threadpool::spawn_many` benchmark when compared to
master. Therefore, I made a few additional changes to lessen the impact.
Through testing, I found that increasing the number of spins before a
worker sleeps from 4 to 60, and changing the test for maximum spins to
use bitwise-and rather than less-than-or-equal (see
https://github.com/tokio-rs/tokio/pull/470#discussion_r201812967) both
resulted in noticeable performance improvements. All these changes had
minor positive impact on the `threadpool::yield_many` benchmark.

## Benchmarks

Performed with rustc 1.28.0 on macOS, macOS 10.12.6 (16G1510), 3.1 GHz
i7-7920HQ. All diffs are expressed against the master version.

```
master:
                               ns/iter
threadpool::spawn_many       3,122,457
threadpool::yield_many      15,118,072

after removing the yield:
                               ns/iter    diff ns/iter    diff %
threadpool::spawn_many       5,047,697     +1,925,240    +61.65%
threadpool::yield_many      13,645,338     -1,472,734     -9.74%

after increasing MAX_SPINS to 60:
                               ns/iter    diff ns/iter    diff %

tokio-threadpool/src/worker/mod.rs
@@ -25,7 +25,6 @@ use std::marker::PhantomData;
 use std::rc::Rc;
 use std::sync::atomic::Ordering::{AcqRel, Acquire};
 use std::sync::Arc;
-use std::thread;
 use std::time::{Duration, Instant};
 
 /// Thread worker
@@ -217,7 +216,7 @@ impl Worker {
     ///
     /// This function blocks until the worker is shutting down.
     pub fn run(&self) {
-        const MAX_SPINS: usize = 3;
+        const MAX_SPINS: usize = 63;
         const LIGHT_SLEEP_INTERVAL: usize = 32;
 
         // Get the notifier.
@@ -261,9 +260,8 @@ impl Worker {
 
             spin_cnt += 1;
 
-            // Yield the thread several times before it actually goes to sleep.
-            if spin_cnt <= MAX_SPINS {
-                thread::yield_now();
+            // Spin the thread several times before it actually goes to sleep.
+            if spin_cnt & MAX_SPINS == spin_cnt {
                 continue;
             }
 